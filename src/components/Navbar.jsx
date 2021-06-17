5476efa6c3f50b200c79df71472a70e1489bc496
Bilikis Sabitu <sabitubilikis96@gmail.com>
2021-06-17 06:12:42 +0100

    updated Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,53 +1,38 @@
 import { useContext } from 'react';
 import { AppContext } from './StateProvider';
-import { Link, useHistory } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 
 // import styles
 import styles from '../styles/Navbar.module.css';
 
-const Navbar = () => {
-	const { state, setState } = useContext(AppContext);
-	const history = useHistory();
+function Navbar() {
+	const context = useContext(AppContext);
 
 	const logout = () => {
-		setState(prev => {
-			return {
-				...prev,
-				isLoggedIn: false,
-				userEmail: null,
-				userId: null,
-			};
+		context.dispatch({
+			type: 'LOGOUT',
 		});
-		history.push('/home');
 	};
 
 	return (
 		<nav className={styles.navbar}>
-			
-			<Link className={styles.navlink} to='/home'>
-				Home
-			</Link>
-			{!state.isLoggedIn ? (
+			{context.state.isUserLoggedIn ? (
 				<>
-					<Link className={styles.navlink} to='/register'>
-						Register
-					</Link>
-					<Link className={styles.navlink} to='/login'>
-						Login
-					</Link>
+					<Link className={styles.navlink} to='/home'>Home</Link>
+					
+					<Link className={styles.navlink} to='/todo'>My Todo</Link>
+					
+					<span onClick={logout}>Logout</span>
 				</>
 			) : (
 				<>
-					<Link className={styles.navlink} to='/todo'>
-						Create Todo
-					</Link>
-					<span onClick={logout} className={styles.navlink}>
-						Logout
-					</span>
+					<Link className={styles.navlink} to='/login'>Login</Link>
+					
+					<Link className={styles.navlink} to='/register'>Register</Link>
+					
 				</>
 			)}
 		</nav>
 	);
-};
-
+}
 export default Navbar;
