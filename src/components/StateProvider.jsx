12dc8d4349024be78d30b9923c46c8d05c3d638a
Bilikis Sabitu <sabitubilikis96@gmail.com>
2021-06-17 06:14:10 +0100

    update Stateprovider

--- src/components/StateProvider.jsx
+++ src/components/StateProvider.jsx
@@ -1,21 +1,74 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useReducer } from 'react';
 
 export const AppContext = createContext();
 
+// reducer function
+function reducer(state, action) {
+	// create a copy of your state
+	let stateCopy = { ...state };
+
+	// set the name on our state copy to action
+	stateCopy.action = action;
+
+	// if action.type is ADD_ITEM
+	// add the payload to shoppingList
+	if (action.type === 'ADD_ITEM') {
+		stateCopy.todoList.unshift(action.payload);
+	}
+
+	// if action.type is LOGIN
+	// set isUserLoggedIn to true
+	// & set userData to payload
+	if (action.type === 'LOGIN') {
+		stateCopy.isUserLoggedIn = true;
+		stateCopy.userData = action.payload;
+	}
+
+	// if action.type is LOGOUT
+	// set isUserLoggedIn to false
+	// & set userData to null
+	if (action.type === 'LOGOUT') {
+		stateCopy.isUserLoggedIn = false;
+		stateCopy.userData = null;
+	}
+
+	return stateCopy;
+}
+
 const initialState = {
-	isLoggedIn: false,
-	userId: null,
-	userEmail: null,
-	posts: [],
+	shoppingList: [
+		{
+			id: 1,
+			title: 'Item 1',
+			description:
+				'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime, totam!',
+		},
+		{
+			id: 2,
+			title: 'Item 2',
+			description:
+				'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime, totam!',
+		},
+	],
+	isUserLoggedIn: false,
+	userData: null,
 };
 
-export default function StateProvider({ children }) {
-	const [appData, setAppData] = useState(initialState);
+function StateProvider({ children }) {
+	// const [state, setState] = useState('');
 
-	
+	const [appstate, dispatch] = useReducer(reducer, initialState);
+
+	const contextObject = {
+		state: appstate,
+		dispatch: dispatch,
+	};
+
 	return (
-		<AppContext.Provider value={{ state: appData, setState: setAppData }}>
+		<AppContext.Provider value={contextObject}>
 			{children}
 		</AppContext.Provider>
 	);
 }
+
+export default StateProvider;
