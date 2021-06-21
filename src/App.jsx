56c8e790f840907892c383bd5476d38d868b8d40
Bilikis Sabitu <sabitubilikis96@gmail.com>
2021-06-21 05:04:50 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Register from './pages/Register';
 import Login from './pages/Login';
-import Home from './pages/Home'; 
-import Todo from './components/Todo';
+
+import TypeNote from './components/TypeNote';
 import Navbar from './components/Navbar';
 import StateProvider from './components/StateProvider';
 
 
-
+//import styles 
+import './index.css';
 
 const App = () => {
   return (
@@ -26,13 +27,10 @@ const App = () => {
             <Route exact path='/login'>
                 <Login />
             </Route>
-            {/* render home Component when user hit /home */}
-            <Route exact path='/home'>
-                <Home />
-            </Route>
+          
             {/* render Todo Component when user hit create todo */}
-            <Route exact path='/todo'>
-                <Todo />
+            <Route exact path='/typenote'>
+                <TypeNote />
             </Route>
 			
         </Switch>
